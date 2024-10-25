3709075d4db54e9b9d1bd807bb5759f5e76d34aa
0x54 <myg0tm3dia@gmail.com>
2024-10-25 10:34:18 -0400

    Add pretty option for CLI docs which uses tables (#6)
    * Add pretty option for CLI docs which uses tables
    * fix bug
    * add pretty tests

--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,8 @@
+| Example Column 1 | Example Column 2 | Example Column 3 |
+|------------------|------------------|------------------|
+| Example Value 1  | Example Value 2  | Example Value 3  |
+| Example Value 4  | Example Value 5  | Example Value 6  |
+| Example Value 7  | Example Value 8  | Example Value 9  |
+
+
 --8<-- "README.md"
--- src/mkdocs_typer2/plugin.py
+++ src/mkdocs_typer2/plugin.py
@@ -1,18 +1,21 @@
 from mkdocs.plugins import BasePlugin
+from mkdocs.config import config_options
 
 from .markdown import makeExtension
 
 
 class MkdocsTyper(BasePlugin):
-    # config_scheme = (
-    #     (
-    #         "cmd",
-    #         config_options.Type(str, default="typer . utils docs"),
-    #     ),
-    # )
+    config_scheme = (
+        (
+            "pretty",
+            config_options.Type(bool, default=False),
+        ),
+    )
 
     def on_config(self, config, **kwargs) -> dict:
-        config["markdown_extensions"].append(makeExtension())
+        config["markdown_extensions"].append(
+            makeExtension(pretty=self.config.get("pretty", False))
+        )
         return config
 
     def on_pre_build(self, config, **kwargs) -> None:
